4353b2999b438f053fd9a4a20fcf9035513b8ec5
Daniil Kikimov <katsushooter@gmail.com>
2023-07-24 21:29:48 +0600

    unnecessary database initialization

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5"
+	"log"
 )
 
 func New(url string) (*pgx.Conn, error) {
+	if len(url) == 0 {
+		log.Printf("No database url provided, skipping database initialization")
+		return nil, nil
+	}
+
 	conn, err := pgx.Connect(context.Background(), url)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to database: %v", err)
